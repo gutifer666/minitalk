3afcc655b350943c736a3ed04a34985e31e15d89
Francisco Javier Gutierrez Perez <frgutier@c3r5s3.42malaga.com>
2022-12-08 18:50:58 +0100

    separate bonus

--- Makefile
+++ Makefile
@@ -6,25 +6,34 @@
 #    By: frgutier <frgutier@student.42malaga.com>   +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/20 17:41:16 by frgutier          #+#    #+#              #
-#    Updated: 2022/11/20 17:53:28 by frgutier         ###   ########.fr        #
+#    Updated: 2022/12/08 18:12:32 by frgutier         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 SOURCES = server.c client.c
 OBJECTS = $(SOURCES:.c=.o)
 
+BONUS_SOURCES = server_bonus.c client_bonus.c
+BONUS_OBJECTS = $(BONUS_SOURCES:.c=.o)
+
 CC = gcc
 CFLAGS = -Wall -Wextra -Werror
 
 all: server client
 
-bonus: server client
+bonus: server_bonus client_bonus
 
 server: server.o libft
 	$(CC) -o $@ $< -Llibft -lft
 
 client: client.o libft
 	$(CC) -o $@ $< -Llibft -lft
+
+server_bonus: server_bonus.o libft
+	$(CC) -o server $< -Llibft -lft
+
+client_bonus: client_bonus.o libft
+	$(CC) -o client $< -Llibft -lft
 
 %.o: %.c
 	$(CC) -c $(CFLAGS) $?
@@ -33,11 +42,11 @@ libft:
 	make -C libft
 
 clean:
-	rm -f $(OBJECTS)
+	rm -f $(OBJECTS) $(BONUS_OBJECTS)
 	make -C libft clean
 	
 fclean: clean
-	rm -f server client libft/libft.a
+	rm -f server client server_bonus client_bonus libft/libft.a
 
 re: fclean all
 
--- client_bonus.c
+++ client_bonus.c
@@ -1,18 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: frgutier <frgutier@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/02 07:32:50 by frgutier          #+#    #+#             */
-/*   Updated: 2022/12/08 18:23:20 by frgutier         ###   ########.fr       */
+/*   Created: 2022/12/08 18:09:32 by frgutier          #+#    #+#             */
+/*   Updated: 2022/12/08 18:10:00 by frgutier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <signal.h>
 #include <stdlib.h>
+
+static void	server_signal_handler(int server_signal)
+{
+	static int	signals_received = 0;
+
+	if (server_signal == SIGUSR1)
+		signals_received++;
+	else
+	{
+		ft_printf("%d\n", signals_received);
+		exit(0);
+	}
+}
 
 static void	send_string(int pid, char *str)
 {
@@ -21,7 +34,7 @@ static void	send_string(int pid, char *str)
 
 	while (*str)
 	{
-		bit = 7;
+		bit = 8;
 		c = *str++;
 		while (bit--)
 		{
@@ -32,7 +45,7 @@ static void	send_string(int pid, char *str)
 			usleep(100);
 		}
 	}
-	bit = 7;
+	bit = 8;
 	while (bit--)
 	{
 		kill(pid, SIGUSR1);
@@ -44,6 +57,12 @@ int	main(int argc, char **argv)
 {
 	if (argc != 3 || !ft_strlen(argv[2]))
 		return (1);
+	ft_printf("Caracteres enviados : %d\n", ft_strlen(argv[2]));
+	ft_printf("Caracteres recibidos: ");
+	signal(SIGUSR1, server_signal_handler);
+	signal(SIGUSR2, server_signal_handler);
 	send_string(ft_atoi(argv[1]), argv[2]);
+	while (1)
+		pause();
 	return (0);
 }
